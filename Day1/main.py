fa08f6197eba7c3dc4018397dc02a82eceef21ac
Dungan, Bertrand (Data61, Fortitude Val.) <Bertrand.Dungan@data61.csiro.au>
2022-12-02 22:58:30 +1000

    Day1 code

---
+++ Day1/main.py
@@ -0,0 +1,25 @@
+from io import TextIOWrapper
+from pathlib import Path
+from functools import reduce
+from typing import Iterable
+
+
+def getTotalCalories(file: TextIOWrapper) -> Iterable[int]:
+    caloriesList = []
+    numRegister = 0
+    for line in file:
+        if line == "\n":
+            caloriesList.append(numRegister)
+            numRegister = 0
+        else:
+            numRegister += int(line)
+    return caloriesList
+
+
+dataPath = Path(__file__).with_name("Data.txt")
+with open(dataPath) as dataFile:
+    elfCalorieList = getTotalCalories(dataFile)
+    print(f"Highest elf calories = {reduce(max, elfCalorieList)}")
+    sortedElfCalories = sorted(elfCalorieList, reverse=True)
+    topThreeElfCalories = sum(sortedElfCalories[0:3])
+    print(f"Top three elf calories combined = {topThreeElfCalories}")
