95dfd0d731434d7af5c56e3d491f4a5ae955636a
Bertrand Dungan <bertranddungan@gmail.com>
2022-12-07 16:58:01 +1000

    WIP

--- Day7/main.py
+++ Day7/main.py
@@ -1,9 +1,38 @@
 from __future__ import annotations
 
-File = str
+from pathlib import Path
+
+
+class File(object):
+    def __init__(self, name: str, size: int) -> None:
+        self.name = name
+        self.size = size
 
 
 class Directory(object):
-    def __init__(self, parent: None | Directory, children: None | list[File]) -> None:
+    def __init__(
+        self, parent: None | Directory = None, files: None | list[File] = None
+    ) -> None:
         self.parent = parent
-        self.children = children
+        self.files = files
+
+    def addFile(self, file: File) -> None:
+        if self.files is not None:
+            self.files.append(file)
+        else:
+            self.files = [file]
+
+
+def isFile(command: str) -> bool:
+    return False
+
+
+def performCommand(line: str, directory: Directory) -> None:
+    pass
+
+
+dataPath = Path(__file__).with_name("Data.txt")
+with open(dataPath) as dataFile:
+    baseDirectory = Directory()
+    for line in dataFile:
+        performCommand(line, baseDirectory)
